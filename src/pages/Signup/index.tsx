77f87288bf8d07f54f997659a7905f2721f8424a
Gabriel Duarte <gabriel.gd7@outlook.com>
2024-05-14 11:48:18 -0300

    signup handle submit

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -8,18 +8,31 @@ import {
   BackToSignInTitle,
   Icon,
 } from "./styles";
-import InputText from "../../components/form/InputText";
+import InputControl from "../../components/form/InputControl";
 import { ScrollView, KeyboardAvoidingView, Platform } from "react-native";
 import Button from "../../components/form/Button";
 import ImgLogo from "../../../assets/logo.png";
 import { useNavigation } from "@react-navigation/native";
+import { useForm, FieldValues } from "react-hook-form";
 
 type NavigationProps = {
   goBack: () => void;
 };
 
+interface IFormFields extends FieldValues {
+  name?: string;
+  email?: string;
+  password?: string;
+}
+
 const Signup = (): React.JSX.Element => {
+  const { control, handleSubmit } = useForm<IFormFields>();
   const { goBack } = useNavigation<NavigationProps>();
+
+  const handleSignUp = ({ name, email, password }: IFormFields): void => {
+    console.log(name, email, password);
+  };
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -30,10 +43,10 @@ const Signup = (): React.JSX.Element => {
           <Content>
             <Logo source={ImgLogo} />
             <Title>Crie sua Conta</Title>
-            <InputText placeholder="Nome Completo" />
-            <InputText placeholder="Email" />
-            <InputText placeholder="Senha" />
-            <Button text="Criar conta" />
+            <InputControl placeholder="Nome Completo" name="name" control={control} />
+            <InputControl placeholder="Email" name="email" control={control} />
+            <InputControl placeholder="Senha" name="password" control={control} />
+            <Button text="Criar conta" onPress={handleSubmit(handleSignUp)} />
           </Content>
         </Container>
       </ScrollView>
